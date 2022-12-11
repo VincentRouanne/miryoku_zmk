a7e3b6499939cd66e55089e2b857a1b9a7d54d88
VincentRouanne <59823232+VincentRouanne@users.noreply.github.com>
2022-12-11 02:36:52 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -35,11 +35,11 @@ MIRYOKU_X(LOLU,   "Lolu")
 &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp NUM_4,         &kp B,             &u_to_U_BASE,      U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp F,             U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp TAB,           &kp A,             &kp S,             &kp D,             &kp P,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-U_NP,              U_NP,              U_NP,              &kp SPC,           &kp LALT,          U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
+U_NP,              U_NP,              U_NP,              U_LT(U_LOLU, SPC), &kp LALT,          U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
 
 #define MIRYOKU_LAYER_LOLU \
-&kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp NUM_4,         &kp B,             &u_to_U_BASE,      U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp F,             U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp TAB,           &kp A,             &kp S,             &kp D,             &kp P,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-U_NP,              U_NP,              U_NP,              &kp SPC,           &kp LALT,          U_BTN2,            U_BTN1,            U_BTN3,            U_NP,    
+&kp F1,            &kp F2,            &kp F3,            &kp F4,            &kp T,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LS(Q),         &kp LS(W),         &kp LS(E),         &kp LS(R),         &kp U_NA,          U_NU,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+U_NA,              &kp GRAVE,         &kp SPC,           &kp U_NA,          &kp ESC,           U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+U_NP,              U_NP,              U_NP,              U_NA,              U_NA,              U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
