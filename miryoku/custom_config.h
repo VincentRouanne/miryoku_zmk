f8424b74c40a5a40fef8be6e8d96a7eed407a5c6
VincentRouanne <59823232+VincentRouanne@users.noreply.github.com>
2022-12-11 02:16:46 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,3 +3,11 @@
 
 #define MIRYOKU_CLIPBOARD_WIN
 #define MIRYOKU_ALPHAS_DVORAK
+
+
+
+#define MIRYOKU_LAYER_EXTRA \
+&kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp NUM_4,         &kp B,             &u_to_U_BASE,      U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp F,             U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+TAB,               &kp A,             &kp S,             &kp D,             &kp P,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+U_NP,              U_NP,              U_NP,              &kp SPC,           &kp LALT,          U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
