f7444e78ba7d69d2c9dc86c7d6e8d416f7796df7
VincentRouanne <59823232+VincentRouanne@users.noreply.github.com>
2022-12-14 15:09:19 +0100

    custom sym and num layers

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -32,11 +32,22 @@ MIRYOKU_X(LOLU,   "Lolu")
 #define U_LOLU   10
 
 #define MIRYOKU_LAYER_EXTRA \
-&kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp NUM_4,         &kp B,             &u_to_U_BASE,      U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp NUM_4,         &kp P,             &u_to_U_BASE,      U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp F,             U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp TAB,           &kp A,             &kp S,             &kp D,             &kp P,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp TAB,           &kp A,             &kp S,             &kp D,             &kp B,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_NP,              U_LT(U_LOLU, SPC), &kp LALT,          U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
+#define MIRYOKU_ALTERNATIVES_NUM \
+&kp CRRT,          &kp NUM_7,         &kp NUM_8,         &kp NUM_9,         &kp PLUS,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp MINUS,         &kp NUM_4,         &kp NUM_5,         &kp NUM_6,         &kp DLLR,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp ASTRK,         &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp FSLH,          U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DOT,           &kp LT,            &kp GT,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+#define MIRYOKU_LAYER_SYM \
+&kp TILDE,         &kp LBKT,          &kp RBKT,          &kp AMPS,          &kp PIPE,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp UNDER,         &kp LPAR,          &kp RPAR,          &kp PRCT,          &kp EQL,           U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp AT,            &kp LBRC,          &kp RBRC,          &kp HASH,          &kp BSLH,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DOT,           &kp EXCL,          &kp QMARK,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_LOLU \
 &kp F1,            &kp F2,            &kp F3,            &kp F4,            &kp T,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
