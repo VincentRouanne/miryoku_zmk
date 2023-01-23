9293ac2a1a12ffa00da345d015e591003c5fa7c9
VincentRouanne <59823232+VincentRouanne@users.noreply.github.com>
2023-01-24 00:44:11 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -37,7 +37,7 @@ MIRYOKU_X(LOLU,   "Lolu")
 &kp TAB,           &kp A,             &kp S,             &kp D,             &kp B,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_NP,              U_LT(U_LOLU, SPC), &kp LALT,          U_BTN2,            U_BTN1,            U_BTN3,            U_NP, 
 
-#define MIRYOKU_LAYER_BASE \
+#define MIRYOKU_LAYER_TAP \
 &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp NUM_4,         &kp P,             &u_to_U_BASE,      U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp F,             U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp TAB,           &kp A,             &kp S,             &kp D,             &kp B,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
