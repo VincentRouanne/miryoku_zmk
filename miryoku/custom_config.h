7c9417ec8003f75fb34580e194d18f631b9bb5dd
Vincent Rouanne <59823232+VincentRouanne@users.noreply.github.com>
2024-06-02 20:25:45 +0200

    Update custom_config.h
    change num layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -51,10 +51,10 @@ U_NP,              U_NP,              U_NP,              U_LT(U_LOLU, SPC), &kp 
 U_NP,              U_NP,              U_NP,              U_LT(U_LOLU, SPC), &kp LALT,          U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
-&kp GRAVE,         &kp NUM_7,         &kp NUM_8,         &kp NUM_9,         &kp PLUS,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-&kp MINUS,         &kp NUM_4,         &kp NUM_5,         &kp NUM_6,         &kp DLLR,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp ASTRK,         &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp FSLH,          U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
-U_NP,              U_NP,              &kp DOT,           &kp NUM_0,         &kp CRRT,          U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+&kp GRAVE,         &kp NUM_9,         &kp NUM_8,         &kp NUM_7,         &kp PLUS,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp MINUS,         &kp NUM_3,         &kp NUM_2,         &kp NUM_1,         &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp ASTRK,         &kp NUM_6,         &kp NUM_5,         &kp NUM_4,         &kp FSLH,          U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DOT,           &kp NUM_0,         &kp DLLR,          U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
 &kp TILDE,         &kp LBKT,          &kp RBKT,          &kp AMPS,          &kp PIPE,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
@@ -65,5 +65,5 @@ U_NP,              U_NP,              &kp DOT,           &kp QMARK,         &kp 
 #define MIRYOKU_LAYER_LOLU \
 &kp F1,            &kp F2,            &kp F3,            &kp F4,            &kp SPC,           U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LS(Q),         &kp LS(W),         &kp LS(E),         &kp LS(R),         &kp U_NA,          U_NU,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-U_NA,              &kp GRAVE,         &kp T,             &kp U_NA,          &kp ESC,           U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp TAB,           &kp GRAVE,         &kp T,             &kp U_NA,          &kp ESC,           U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_NP,              U_NA,              U_NA,              U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
